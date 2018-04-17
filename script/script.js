fbb20102d674466e6e9ba0476a57b560411487ac
MiKaminskas <zeniptz@gmail.com>
2018-04-17 14:39:32 +0300

    mobile version complete!

--- script/script.js
+++ script/script.js
@@ -1,1 +1,148 @@
-console.log("hello");
+console.log("Desktop script run");
+'use strict';
+var fullObj;
+//start point
+desktopInit();
+
+function desktopInit() {
+    if (false) {
+        tableOnClick(14);
+    }
+    else {
+        VK.init(function () {
+            // API initialization succeeded
+            // Your code here
+            console.log("Vk loaded");
+            let real_height = 4000;
+            VK.callMethod('resizeWindow', 1000, real_height);
+            tableOnClick(14);
+            //real_height = document.getElementById('page').clientHeight;
+            //VK.callMethod('resizeWindow', 1000, real_height);
+        }, function () {
+            // API initialization failed
+            // Can reload page here
+            tableOnClick(14);
+        }, '5.74');
+    }
+}
+
+
+
+
+function tableOnClick(idClub) {
+    //let url = 'http://fitnes-ptz.ru/mobile/widget.php';
+    //let url = 'https://webapp.tsdames.lt/index.php?url=http://fitnes-ptz.ru/mobile/widget.php';
+    let url2 = 'https://webapp.tsdames.lt/index.php?url=http://fitnes-ptz.ru/mobile/widget2.php';
+    fetch(url2)
+        .then((res) => {
+
+            return res.json();
+        })
+        .then((out) => {
+            fullObj = out;
+            createTable(getClubs(out, idClub));
+        })
+        .catch(err => {
+            throw err
+        });
+}
+
+function createTable(sortedArray) {
+    let tbBody = document.getElementById("tablebody");
+    let thisWeek = [];
+    /*thisWeek.push(sortedArray[0]);*/
+    for (let mainCounter = 1; mainCounter < sortedArray.length - 1; mainCounter++) {
+        thisWeek.push(sortedArray[mainCounter - 1]);
+        for (var i = mainCounter; i < sortedArray.length; i++) {
+            mainCounter = i;
+            if (sortedArray[i].time == sortedArray[i - 1].time) {
+                thisWeek.push(sortedArray[i]);
+                /*mainCounter=i;*/
+            } else {
+                break;
+            }
+
+        }
+
+        let trNew = document.createElement("tr");
+        trNew.style.maxWidth = "1000px";
+        let text = "";
+        let thisDayCounter = 0;
+        for (let j = 0; j < 8; j++) {
+            let td = document.createElement("td");
+
+            if (j === 0) {
+                let timeText = document.createElement("span");
+                timeText.innerText = thisWeek[0].time;
+                timeText.style.width = "8%";
+                td.appendChild(timeText);
+                td.style.width = "8%";
+            } else {
+
+                for (let k = 0; k < thisWeek.length; k++) {
+
+                    if (thisWeek[k].day == j) {
+                        if (thisDayCounter > 0) {
+                            td.appendChild(document.createElement("hr"));
+                        }
+                        let classNameText = document.createElement("span");
+                        classNameText.innerText = thisWeek[k].class + " \n";
+                        td.appendChild(classNameText);
+
+                        let teamName = document.createElement("b");
+                        teamName.innerText = thisWeek[k].team + " ";
+                        td.style.width = "13%";
+                        td.appendChild(teamName);
+                        thisDayCounter++;
+                        continue;
+                    }
+
+                }
+            }
+            thisDayCounter = 0;
+            text = "";
+            trNew.appendChild(td)
+        }
+        tbBody.appendChild(trNew);
+        thisWeek = [];
+    }
+
+
+}
+
+function buildTable(text, id, idClub) {
+    let activeList = document.getElementsByClassName("active");
+    activeList[0].classList.remove("active");
+    document.getElementById("title").innerText = text;
+    id.classList.add("active");
+    clearTable();
+    tableOnClick(idClub);
+}
+
+
+function clearTable() {
+    document.getElementById("tablebody").innerHTML = "";
+
+}
+
+var getClubs = function (jsonObj, idClub) {
+    let arrayOfClub = [], k = 0;
+    console.log("hello form getClassesFromClub  " + jsonObj.schedule.length);
+    for (let i = 0; i < jsonObj.schedule.length; i++) {
+        let currObj = jsonObj.schedule[i];
+        if (currObj.clubid == idClub) {
+            arrayOfClub.push(currObj);
+            k++;
+        }
+    }
+    console.log(arrayOfClub.sort(compare));
+    return arrayOfClub;
+};
+
+function compare(a, b) {
+    if (a.time < b.time)
+        return -1;
+    if (a.time > b.time)
+        return 1;
+    return 0;
+}
---
+++ script/scriptMobile.js
@@ -0,0 +1,154 @@
+console.log("Mobile script run");
+var fullObj;
+mobileInit();
+
+function mobileInit() {
+    if (false) {
+        tableOnClick(14);
+    }
+    else {
+        VK.init(function () {
+            // API initialization succeeded
+            // Your code here
+            console.log("Vk loaded");
+            let real_height = 4000;
+            VK.callMethod('resizeWindow', 1000, real_height);
+            tableOnClick(14);
+            //real_height = document.getElementById('page').clientHeight;
+            //VK.callMethod('resizeWindow', 1000, real_height);
+        }, function () {
+            // API initialization failed
+            // Can reload page here
+            tableOnClick(14);
+        }, '5.74');
+    }
+}
+
+function tableOnClick(idClub) {
+    //let url = 'http://fitnes-ptz.ru/mobile/widget.php';
+    //let url = 'https://webapp.tsdames.lt/index.php?url=http://fitnes-ptz.ru/mobile/widget.php';
+    let url2 = 'https://webapp.tsdames.lt/index.php?url=http://fitnes-ptz.ru/mobile/widget2.php';
+    fetch(url2)
+        .then((res) => {
+
+            return res.json();
+        })
+        .then((out) => {
+            fullObj = out;
+            createTable(getClassesFromClub(out, idClub));
+        })
+        .catch(err => {
+            throw err
+        });
+}
+
+function getClassesFromClub(jsonObj, idClub) {
+    let arrayOfClub = [];
+    console.log("hello form getClassesFromClub  " + jsonObj.schedule.length);
+    for (let i = 0; i < jsonObj.schedule.length; i++) {
+        let currObj = jsonObj.schedule[i];
+        if (currObj.clubid == idClub) {
+            arrayOfClub.push(currObj);
+
+        }
+    }
+    console.log(arrayOfClub.sort(compareByDay));
+    return arrayOfClub;
+}
+
+function compareByDay(a, b) {
+    if (a.day < b.day)
+        return -1;
+    if (a.day > b.day)
+        return 1;
+    return 0;
+}
+function compareByTime(a,b) {
+    if (a.time < b.time)
+        return -1;
+    if (a.time > b.time)
+        return 1;
+    return 0;
+}
+
+function dayByDay(sortedArray) {
+    let thisWeek = [];
+    thisWeek.push(sortedArray[0]);
+    for (let i = 1; i < sortedArray.length; i++) {
+        if (thisWeek[i].day == sortedArray[i]) {
+            thisWeek.push(sortedArray[i]);
+        }
+    }
+
+}
+
+function createTable(sortedArray) {
+    let tbBody = document.getElementById("tablebody");
+    console.log("createTable");
+    let thisDay = [];
+    for (let i = 1; i < sortedArray.length - 1 ; i++) {
+        thisDay.push(sortedArray[i-1]);
+        for (let j = i;j<sortedArray.length;j++){
+            i = j;
+            if (sortedArray[j].day == sortedArray[j-1].day){
+                thisDay.push(sortedArray[j]);
+            } else {
+                break;
+            }
+        }
+
+        thisDay.sort(compareByTime);
+        for (let k =0;k<thisDay.length;k++) {
+            console.log(thisDay);
+            let trNew = document.createElement("tr");
+
+            let tdDay = document.createElement("td");
+            tdDay.innerText = convertDay(thisDay[k].day);
+            trNew.appendChild(tdDay);
+
+            let tdTime = document.createElement("td");
+            tdTime.innerText = thisDay[k].time;
+            trNew.appendChild(tdTime);
+
+            let tdClass = document.createElement("td");
+            tdClass.innerText = thisDay[k].class + "\n" + thisDay[k].team;
+            trNew.appendChild(tdClass);
+
+            tbBody.appendChild(trNew);
+        }
+        thisDay =[];
+
+    }
+}
+
+function convertDay(day) {
+    switch (day) {
+        case "1":
+            return "Понедельник";
+        case "2":
+            return "Вторник";
+        case "3":
+            return "Среда";
+        case "4":
+            return "Четверг";
+        case "5":
+            return "Пятница";
+        case "6":
+            return "Суббота";
+        case "7":
+            return "Воскресенье";
+    }
+}
+
+function buildTable(text, id, idClub) {
+    let activeList = document.getElementsByClassName("active");
+    activeList[0].classList.remove("active");
+    document.getElementById("title").innerText = text;
+    id.classList.add("active");
+    clearTable();
+    tableOnClick(idClub);
+}
+
+function clearTable() {
+    document.getElementById("tablebody").innerHTML = "";
+}
